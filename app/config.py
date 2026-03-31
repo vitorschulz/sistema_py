7421a96a77ae328f950925bbdb98140828220a0a
Vitor Schulz <vitorschulz123321@gmail.com>
2026-03-31 02:13:16 -0300

    dockando

--- app/config.py
+++ app/config.py
@@ -1,10 +1,11 @@
+import os
 import mysql.connector
 
 def get_db_connection():
     conn = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="gestao"
+        host=os.getenv("DB_HOST") or "localhost",
+        user=os.getenv("DB_USER") or "root",
+        password=os.getenv("DB_PASSWORD") or "",
+        database=os.getenv("DB_NAME") or "gestao"
     )
     return conn
