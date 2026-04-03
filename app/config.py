6636d5d43bd9ad839bc8dcbeae80b94efd8ac052
Vitor Schulz <vitorschulz123321@gmail.com>
2026-04-03 20:17:10 -0300

    arrumanmdo conexao

--- app/config.py
+++ app/config.py
@@ -1,11 +1,20 @@
 import os
 import mysql.connector
+import time
 
 def get_db_connection():
-    conn = mysql.connector.connect(
-        host=os.getenv("DB_HOST") or "localhost",
-        user=os.getenv("DB_USER") or "root",
-        password=os.getenv("DB_PASSWORD") or "",
-        database=os.getenv("DB_NAME") or "gestao"
-    )
-    return conn
+    for i in range(10):
+        try:
+            conn = mysql.connector.connect(
+                host=os.getenv("DB_HOST") or "localhost",
+                user=os.getenv("DB_USER") or "root",
+                password=os.getenv("DB_PASSWORD") or "",
+                database=os.getenv("DB_NAME") or "gestao"
+            )
+            print("Conectou no MySQL!")
+            return conn
+        except mysql.connector.Error:
+            print("MySQL não disponível ainda, tentando novamente...")
+            time.sleep(3)
+
+    raise Exception("Não conseguiu conectar no MySQL")
